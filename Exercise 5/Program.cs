71b6b9ae0484869333a5af49af8cbb3c99fc8ebb
Alimkz18 <alim.zhainakov@gmail.com>
2024-06-22 13:48:41 -0500

    Add project files.

---
+++ Exercise 5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Exercise_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            WriteLine("Enter number of lines: ");
+            int lines = int.Parse(ReadLine());
+
+            WriteLine("Enter number of columns: ");
+            int col = int.Parse(ReadLine());
+
+            int[,] matrix = new int[lines, col];
+
+            WriteLine();
+
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = r.Next(0, 10);
+                    Write($"{matrix[i, j]} ");
+                }
+                WriteLine();
+            }
+
+            WriteLine();
+
+            WriteLine("Enetr number to find: ");
+            int num = int.Parse(ReadLine());
+
+            int find = 0;
+
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if (matrix[i, j] == num) find++;
+                }
+            }
+
+            WriteLine($"Number repeates {find} times ");
+
+
+            ReadLine();
+        }
+    }
+}
---
+++ Matrix Addition/Program.cs
@@ -0,0 +1,58 @@
+﻿using static System.Console;
+
+namespace Matrix_Addition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] A = {
+            {1, 2, 3},
+            {4, 5, 6}
+       
+            };
+
+            int[,] B = {
+            {7, 8, 9},
+            {10, 11, 12}
+       
+            };
+
+            int[,] C = new int[2, 3];
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    C[i, j] = A[i, j] + B[i, j];
+                    WriteLine($"{A[i, j]} + {B[i, j]} = {C[i, j]} ");
+                }
+            }
+
+            WriteLine("Matrix C = ");
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Write($"{C[i, j]} ");
+                }
+                WriteLine();
+            }
+
+            ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
